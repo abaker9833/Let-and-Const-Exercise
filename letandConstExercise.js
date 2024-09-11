179df1fc4ef5421baa3b8a2989b3bb202d69b114
abaker9833 <149739445+abaker9833@users.noreply.github.com>
2024-09-11 13:30:15 -0500

    Add files via upload

---
+++ letandConstExercise.js
@@ -0,0 +1,17 @@
+const PI = 3.14;
+
+
+//What is the difference between var and let?
+//Var can be used throughout the whole function to declare new variables but let is only allowed to declare new variables if they are within the same block.
+
+
+//What is the difference between var and const?
+//Var allows users to redeclare while const cannot be redeclared and is confined to a block.
+
+
+//What is the difference between let and const?
+//While both are confined to a block, let can be changed but const can not.
+
+
+//What is hoisting?
+//Before any code is executed, declarations (variables, functions, classes, etc.) are moved to the top of their current scope.
